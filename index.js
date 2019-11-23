fe7751598a550f0e33e8615a2d63e85fbbcab1b7
nevikw39 <nevik.w39@gmail.com>
2019-11-23 19:07:55 +0800

    Init

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client } = require('libfb')
 const config = require("./config.js");
 const client = new Client()
+
 client.login(config.get("user"), config.get("pass")).then(() => {
     client.on('message', message => {
-        console.log('Got a message!')
-        console.log(message.message)
+        console.log(message)
         client.sendMessage(message.threadId, message.message)
     })
 })

0a5fe68914e711c0775637a203333095dbd769be
nevikw39 <nevik.w39@gmail.com>
2019-11-27 22:38:18 +0800

    alpha bin

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
+const fs = require('fs');
 const { Client } = require('libfb');
-const Line = require('/usr/local/lib/node_modules/linebot');
-const config = require("./config.js");
+const Line = require('linebot');
+const config = require('config');
 
-const messenger = new Client();
-messenger.login(config.get(["messenger", "usr"]), config.get(["messenger", "pwd"])).then(() => {
+
+
+const messenger = fs.existsSync('session.json') ? new Client({ session: JSON.parse(fs.readFileSync('session.json', "utf8")) }) : new Client();
+
+messenger.login(config.messenger.usr, config.messenger.pwd).then(() => {
+
     messenger.on('message', msg => {
-        if (msg.threadId != "100003690746495") return;
-        console.log(msg);
-        messenger.sendMessage(msg.threadId, msg.message);
-    })
+        console.debug(msg);
+        const lid = config.ml[msg.threadId];
+        if (lid == undefined) return;
+        line.push(lid, msg.message);
+    });
+
+    const session = messenger.getSession();
+    fs.writeFileSync("session.json", JSON.stringify(session));
+
 }).catch(err => console.error(err));
 
+
+
 const line = Line({
-    channelId: config.get(["line", "id"]),
-    channelSecret: config.get(["line", "secret"]),
-    channelAccessToken: config.get(["line", "token"])
+    channelId: config.line.id,
+    channelSecret: config.line.secret,
+    channelAccessToken: config.line.token
 });
+
+line.on('join', event => {
+    console.debug(event);
+})
+
 line.on('message', event => {
-    console.log(event);
-    let msg = event.message.text;
-    event.reply(msg);
+    console.debug(event);
+    const mid = config.lm[event.source.groupId];
+    if (event.message.type != "text" || mid == undefined) return;
+    messenger.sendMessage(mid, event.message.text).catch(err => console.error(err));
 });
+
 line.listen('/linewebhook', 3000, function () {
 });

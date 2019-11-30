a0f5381a21c5f926dfe9dcf42227752a671fbe4e
nevikw39 <nevikw39@icloud.com>
2019-12-01 00:17:17 +0800

    alpha tri

--- index.js
+++ index.js
@@ -9,11 +9,62 @@ const messenger = new Client(fs.existsSync('session.json') ? { session: JSON.par
 
 messenger.login(config.messenger.usr, config.messenger.pwd).then(() => {
 
-    messenger.on('message', msg => {
-        console.debug(msg);
-        const lid = config.ml[msg.threadId];
+    messenger.on('message', async event => {
+        console.debug(event);
+        let that = this;
+        if (this.id != null) {
+            if (this.id == event.authorId)
+                return;
+            else
+                this.id = null;
+        }
+        config.cars.forEach(i => {
+            if (event.message.includes(i))
+                that.id = event.authorId;
+        });
+        if (this.id != null)
+            return;
+        const lid = config.ml[event.threadId];
         if (lid == undefined) return;
-        line.push(lid, msg.message);
+        const usr = await messenger.getUserInfo(event.authorId);
+        const msg = {
+            "type": "flex",
+            "altText": "From Messenger",
+            "contents": {
+                "type": "bubble",
+                "header": {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                        {
+                            "type": "icon",
+                            "url": usr.profilePicSmall,
+                            "size": "sm"
+                        },
+                        {
+                            "type": "text",
+                            "weight": "bold",
+                            "color": "#198964",
+                            "margin": "xxl",
+                            "text": usr.name,
+                            "size": "sm"
+                        }
+                    ]
+                },
+                "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": event.message,
+                            "size": "md"
+                        }
+                    ]
+                }
+            }
+        };
+        line.push(lid, [msg]).catch(err => console.error(err));
     });
 
     const session = messenger.getSession();
@@ -42,13 +93,17 @@ line.on('message', event => {
             line.reply(event.replyToken, { type: "text", text: JSON.stringify(eval(msg.substring(msg.indexOf(' ')))) });
             return;
         case "/connect":
-            config.ml[msg.substring(msg.indexOf(' '))] = event.source.groupId;
-            config.lm[event.source.groupId] = msg.substring(msg.indexOf(' '));
+            config.ml[msg.substring(msg.indexOf(' ') + 1)] = event.source.groupId;
+            config.lm[event.source.groupId] = msg.substring(msg.indexOf(' ') + 1);
             return;
     }
     if (event.message.type != "text" || mid == undefined) return;
     messenger.sendPresenceState(mid);
-    messenger.sendMessage(mid, msg).catch(err => console.error(err));
+    line.getUserProfile(event.source.userId).then(p => {
+        messenger.sendMessage(mid, `*${p.displayName}*`);
+        messenger.sendPresenceState(mid);
+        messenger.sendMessage(mid, msg).catch(err => console.error(err));
+    });
 });
 
 line.listen('/linewebhook', 3000, function () {
